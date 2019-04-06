65b8a81d88cc175772e353f2fe3edfc22902463a
Faith <13Kurrane@gmail.com>
2019-04-06 15:50:26 +0100

    1.1

--- static/main.js
+++ static/main.js
@@ -1,3 +1,8 @@
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+
 var socket = null;
 var raids = [];
 var raidConfigs = [];
